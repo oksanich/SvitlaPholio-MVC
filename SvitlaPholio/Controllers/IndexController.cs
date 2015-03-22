fedea9078ff5ee2d8bb702a8e6fbb63319dcf161
Aleksandr <oksanich_90@mail.ru>
2015-03-22 14:01:40 +0200

    Created project on MVC.

---
+++ SvitlaPholio/Controllers/IndexController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SvitlaPholio.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult PortfolioPreview()
+        {
+            return View();
+        }
+
+    }
+}
